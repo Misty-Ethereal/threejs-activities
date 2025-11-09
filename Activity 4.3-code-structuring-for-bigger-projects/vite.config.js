d272f114a215ccfa455a7fe8adf66cd4996c88e7
Misty-Ethereal <leamichellemagpatoc10@gmail.com>
2025-11-09 20:03:25 +0800

    Add Activity 4.3 full folder

--- Activity 4.3-code-structuring-for-bigger-projects/vite.config.js
+++ Activity 4.3-code-structuring-for-bigger-projects/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffdfa2f89e3cd75c5ab424abffe04ebc5a2ccc192690ae1924f6dd26110291bc
-size 379
+oid sha256:e042a1d44ca775f2c891d9744b1dcc23f84504bcb2aedcb90ecb475c0d257d05
+size 397
